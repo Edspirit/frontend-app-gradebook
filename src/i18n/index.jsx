0440ddd51a0ae350579398ac41181af9b11c3c86
Emad <codewithemad@gmail.com>
2023-01-21 16:33:05 +0330

    feat: persian translations added

--- src/i18n/index.jsx
+++ src/i18n/index.jsx
@@ -1,12 +1,14 @@
 import arMessages from './messages/ar.json';
 // no need to import en messages-- they are in the defaultMessage field
 import es419Messages from './messages/es_419.json';
+import faIRMessages from './messages/fa_IR.json';
 import frMessages from './messages/fr.json';
 import zhcnMessages from './messages/zh_CN.json';
 
 const messages = {
   ar: arMessages,
   'es-419': es419Messages,
+  fa: faIRMessages,
   fr: frMessages,
   'zh-cn': zhcnMessages,
 };
