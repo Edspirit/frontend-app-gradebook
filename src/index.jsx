7089de828e42c132c050206b5f996a2cf91069af
milad-emami <milad.emami1025@gmail.com>
2023-07-24 14:00:02 +0330

    feat: new env variable added

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,23 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 import {
   APP_READY,
   initialize,
   mergeConfig,
   subscribe,
-} from '@edx/frontend-platform';
-import { messages as headerMessages } from '@edx/frontend-component-header';
-import { messages as footerMessages } from '@edx/frontend-component-footer';
+} from "@edx/frontend-platform";
+import { messages as headerMessages } from "@edx/frontend-component-header";
+import { messages as footerMessages } from "@edx/frontend-component-footer";
 
-import appMessages from './i18n';
-import App from './App';
+import appMessages from "./i18n";
+import App from "./App";
 
 subscribe(APP_READY, () => {
-  ReactDOM.render(<App />, document.getElementById('root'));
+  ReactDOM.render(<App />, document.getElementById("root"));
 });
 
 initialize({
@@ -29,18 +29,16 @@ initialize({
         LOGIN_URL: process.env.LOGIN_URL,
         LOGOUT_URL: process.env.LOGOUT_URL,
         CSRF_TOKEN_API_PATH: process.env.CSRF_TOKEN_API_PATH,
-        REFRESH_ACCESS_TOKEN_ENDPOINT: process.env.REFRESH_ACCESS_TOKEN_ENDPOINT,
+        REFRESH_ACCESS_TOKEN_ENDPOINT:
+          process.env.REFRESH_ACCESS_TOKEN_ENDPOINT,
         DATA_API_BASE_URL: process.env.DATA_API_BASE_URL,
-        SECURE_COOKIES: process.env.NODE_ENV !== 'development',
+        SECURE_COOKIES: process.env.NODE_ENV !== "development",
         SEGMENT_KEY: process.env.SEGMENT_KEY,
         ACCESS_TOKEN_COOKIE_NAME: process.env.ACCESS_TOKEN_COOKIE_NAME,
+        AC_INSTANCE_CONFIG_API_URL: process.env.AC_INSTANCE_CONFIG_API_URL,
       });
     },
   },
-  messages: [
-    appMessages,
-    headerMessages,
-    footerMessages,
-  ],
+  messages: [appMessages, headerMessages, footerMessages],
   requireAuthenticatedUser: true,
 });
