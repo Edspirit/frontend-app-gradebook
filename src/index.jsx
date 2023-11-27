a19a2f97cf1680281592706c278afbe72e9e3ab8
Khaled <info@khaledr.ir>
2023-11-27 08:38:27 +0330

    feat: new env variable added

--- src/index.jsx
+++ src/index.jsx
@@ -29,11 +29,13 @@ initialize({
         LOGIN_URL: process.env.LOGIN_URL,
         LOGOUT_URL: process.env.LOGOUT_URL,
         CSRF_TOKEN_API_PATH: process.env.CSRF_TOKEN_API_PATH,
-        REFRESH_ACCESS_TOKEN_ENDPOINT: process.env.REFRESH_ACCESS_TOKEN_ENDPOINT,
+        REFRESH_ACCESS_TOKEN_ENDPOINT:
+        process.env.REFRESH_ACCESS_TOKEN_ENDPOINT,
         DATA_API_BASE_URL: process.env.DATA_API_BASE_URL,
         SECURE_COOKIES: process.env.NODE_ENV !== 'development',
         SEGMENT_KEY: process.env.SEGMENT_KEY,
         ACCESS_TOKEN_COOKIE_NAME: process.env.ACCESS_TOKEN_COOKIE_NAME,
+        AC_INSTANCE_CONFIG_API_URL: process.env.AC_INSTANCE_CONFIG_API_URL,
       });
     },
   },
